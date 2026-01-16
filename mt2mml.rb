75d4cf689fba6cff631010cbd652dfc668b35ef1
hoangtaybac <165269336+hoangtaybac@users.noreply.github.com>
2026-01-16 13:35:29 +0700

    Update mt2mml.rb

--- mt2mml.rb
+++ mt2mml.rb
@@ -1,22 +1,13 @@
-# mt2mml.rb - MathType OLE(.bin) -> MathML
-require "mathtype_to_mathml"
+# mt2mml.rb - Simple MathType to MathML converter using gem
+require 'mathtype_to_mathml'
 
-module Mt2Mml
-  def self.convert_path(path)
-    converter = MathTypeToMathML::Converter.new(path)
-    converter.convert.to_s
-  end
-end
+path = ARGV[0]
+abort "usage: ruby mt2mml.rb <oleObject*.bin>" unless path && File.exist?(path)
 
-# CLI compatible
-if __FILE__ == $0
-  path = ARGV[0]
-  abort "usage: ruby mt2mml.rb <oleObject*.bin>" unless path && File.exist?(path)
-
-  begin
-    puts Mt2Mml.convert_path(path)
-  rescue => e
-    STDERR.puts "Error: #{e.message}"
-    exit 1
-  end
+begin
+  converter = MathTypeToMathML::Converter.new(path)
+  puts converter.convert
+rescue => e
+  STDERR.puts "Error: #{e.message}"
+  exit 1
 end
